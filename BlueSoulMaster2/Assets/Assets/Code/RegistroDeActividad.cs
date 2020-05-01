ffefac153fa87e17194bb8ad2309cc3c1c24688d
NahomiLeon <63111910+NahomiRyu3095@users.noreply.github.com>
2020-05-01 16:45:27 -0500

    nuevo

---
+++ BlueSoulMaster2/Assets/Assets/Code/RegistroDeActividad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegistroDeActividad : MonoBehaviour
+{
+    // aqui
+    /*
+     hoy Abril 21 el profe reviso le gusto y se agregara la version 3.10
+     abril 24
+     participacion de 
+     Kibsaim
+     Daniel
+     Alexander
+
+
+    abril 25
+    kip
+    alex
+    acomodo del mapa, trigger y respawnPoint
+    */
+
+
+
+}
---
+++ BlueSoulMaster2/Assets/RespawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+
+    void OnTriggerEnter2D(Collider2D RES)
+    {
+        player.transform.position = respawnPoint.transform.position;  //Players postion is the same as respawn position
+    }
+}
